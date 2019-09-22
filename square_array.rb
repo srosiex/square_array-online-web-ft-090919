4de41287847cf7fd130eee4f90a9c6fd6dca5546
Sarah H <sarahrose.henderson@gmail.com>
2019-09-22 17:17:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_array = []
 
-  array.each do {|num|
-  new_array << {num * num}}
+  array.each do |num|
+    new_array << (num * num)
   end
   end
